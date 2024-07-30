bb1194efd68e28ea7c63718af4494ac09373dae8
Alessio "Yagotzirck" Simbula <yagotzirck@gmail.com>
2024-07-30 23:06:41 +0200

    Add the contract's .sol source file

---
+++ contracts/IPFS_Management.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+struct File {
+    uint    fileId;     // An incremental number associated to each loaded file
+    string  fileName;   // "<filename>.<extension>"" format, e.g. "book.pdf" 
+    bytes32 cid;        // The file's CID in the IPFS network
+}
+
+struct User {
+    bool    isAuthorized;  // true if the user has been authorized by the owner
+    File[]  files; 
+}
+
+
+contract IPFS_Management {
+    /****************************************************
+     * Variables
+     ***************************************************/
+
+    // The smart contract's creator / admin
+    address private owner;
+
+    // An incremental ID to assign to each File's "fileId" field
+    uint private currId;
+
+    // A mapping associating each authorized user to its User struct
+    mapping (address => User) private users;
+
+
+    /****************************************************
+     * Modifiers
+     ***************************************************/
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the Smart Contract's owner/creator can call this function"
+        );
+        _;
+    }
+
+    modifier onlyAuthUsers() {
+        require(
+            users[msg.sender].isAuthorized == true,
+            "Only users authorized by the Smart contract's owner can call "
+            "this function"
+        );
+        _;
+    }
+    
+
+    /****************************************************
+     * Functions
+     ***************************************************/
+     constructor() {
+        owner = msg.sender;
+        users[owner].isAuthorized = true;
+     }
+
+     function addUser(address newUser) external onlyOwner {
+        users[newUser].isAuthorized = true;
+     }
+     
+     function addFile(
+        string calldata fileName,
+        bytes32 cid
+     ) external onlyAuthUsers
+     {
+        address userAddr = msg.sender;
+        users[userAddr].files.push(
+            File(
+                currId++,
+                fileName,
+                cid
+            )
+        );
+     }
+
+     function viewFiles()
+     external view onlyAuthUsers
+     returns (File[] memory files)
+     {
+        address userAddr = msg.sender;
+        return users[userAddr].files;
+     }
+}
